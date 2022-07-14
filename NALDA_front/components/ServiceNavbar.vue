160d0d98e80c74845704de102f3b46cbd1b63312
ksh3967 <ksh3967@naver.com>
2022-07-14 21:54:00 +0900

    [S07P11A204-43] feat: snack&drink vue.create

---
+++ NALDA_front/components/ServiceNavbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav>
+      <div id="nav-tab" class="nav nav-tabs" role="tablist">
+        <a
+          id="nav-home-tab"
+          class="nav-item nav-link active"
+          data-toggle="tab"
+          href="#nav-home"
+          role="tab"
+          aria-controls="nav-home"
+          aria-selected="true"
+          >snack</a
+        >
+        <a
+          id="nav-profile-tab"
+          class="nav-item nav-link"
+          data-toggle="tab"
+          href="#nav-profile"
+          role="tab"
+          aria-controls="nav-profile"
+          aria-selected="false"
+          >non-alcohol</a
+        >
+        <a
+          id="nav-contact-tab"
+          class="nav-item nav-link"
+          data-toggle="tab"
+          href="#nav-contact"
+          role="tab"
+          aria-controls="nav-contact"
+          aria-selected="false"
+          >alcohol</a
+        >
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ServiceNavbar',
+  components: {},
+}
+</script>
+
+<style></style>
