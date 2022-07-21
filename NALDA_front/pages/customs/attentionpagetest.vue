f7ddd55a65fd29774044ff2486f737c9cceee541
jung ah <jkimlow@naver.com>
2022-07-21 17:35:04 +0900

    [S07P12A204-56] feat: create customs attention page

---
+++ NALDA_front/pages/customs/attentionpagetest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="overflow-auto">
+    <div>
+      <b-pagination v-model="currentPage" pills :total-rows="rows" size="sm">
+      </b-pagination>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AttentionTest',
+    components: {},
+    data() {
+        return {
+            rows: 2,
+            currentPage: 1
+        }
+    },
+    methods: {
+        paging() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ NALDA_front/pages/customs/form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>입력 폼</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomsForm'
+}
+</script>
+
+<style>
+
+</style>
