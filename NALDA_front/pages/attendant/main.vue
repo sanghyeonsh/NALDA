60ac39220d1c025e123fd410bd7bd8faba0c8d51
jung ah <jkimlow@naver.com>
2022-07-28 17:58:15 +0900

    edit

--- NALDA_front/pages/attendant/main.vue
+++ NALDA_front/pages/attendant/main.vue
@@ -29,7 +29,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .main-container {
   display: flex;
   height: 70vh;
--- NALDA_front/pages/attendant/request.vue
+++ NALDA_front/pages/attendant/request.vue
@@ -98,7 +98,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .request-container {
   height: 70vh;
   display: flex;
