2dd3cb5ce79cdd67dd770f2e678e27a00e744eeb
HYUNGJIN <accent680@naver.com>
2022-07-31 20:30:07 +0900

    add pages/waiting.vue

--- NALDA_front/pages/waiting.vue
+++ NALDA_front/pages/waiting.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
+  <div class="waiting-page">
     <HeaderComponent />
-    <div class="box">
-      <div class="text-box">
-        가져다 드리겠습니다.<br />
-        잠시만 기다려주세요!<br />
+    <div class="waiting-page-body">
+      <div class="waiting-page-body-text-box">
+        <div>
+          가져다 드리겠습니다. <br />
+          잠시만 기다려주세요!
+        </div>
       </div>
+      <button class="waiting-page-body-button">
+        <img src="../static/main/flight_attendant.png" alt="" />
+        승무원
+      </button>
     </div>
-    <FooterComponent />
+    <div class="waiting-footer">
+      <span
+        >이용약관 | 개인정보처리방침 | 책임의 한계와 고지 | 회원정보
+        고객센터</span
+      >
+      <div>Copyright © NALDA Corp. All Rights Reserved.</div>
+    </div>
   </div>
 </template>
 
@@ -18,14 +30,19 @@ export default {
 </script>
 
 <style scoped>
-.box {
+.waiting-page {
+  height: 100vh;
+  width: 100vw;
+}
+.waiting-page-body {
   background-color: white;
   height: 70vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
-.text-box {
+.waiting-page-body-text-box {
   display: flex;
   width: 70vw;
   height: 50vh;
@@ -34,5 +51,27 @@ export default {
   justify-content: center;
   align-items: center;
   font-size: 50px;
+}
+
+.waiting-page-body-button {
+  background-color: rgb(69, 169, 200);
+  position: fixed;
+  bottom: 15vh;
+  right: 2vw;
+  width: 10vw;
+  height: 5vh;
+  border-radius: 60px;
+  border: none;
+}
+.waiting-page-body-button img {
+  width: 30%;
+  height: 100%;
+}
+
+.waiting-footer {
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
