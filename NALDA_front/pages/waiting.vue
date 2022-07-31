6dba6f18a1ff331178f32d9ed8a7ce29d0da37e9
HYUNGJIN <accent680@naver.com>
2022-07-31 19:01:55 +0900

    add waiting page

---
+++ NALDA_front/pages/waiting.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <div class="box">
+      <div class="text-box">
+        가져다 드리겠습니다.<br />
+        잠시만 기다려주세요!<br />
+      </div>
+    </div>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WaitingPage',
+}
+</script>
+
+<style scoped>
+.box {
+  background-color: white;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.text-box {
+  display: flex;
+  width: 70vw;
+  height: 50vh;
+  border: 2px solid black;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+}
+</style>
