3f410567de027b3d889ebbbf853116c708220b4a
HYUNGJIN <accent680@naver.com>
2022-08-06 00:12:51 +0900

    transportation map, clock

--- NALDA_front/components/TransportationSubway.vue
+++ NALDA_front/components/TransportationSubway.vue
@@ -5,20 +5,44 @@
       <div class="terminal-2">제2터미널</div>
     </div>
     <div class="subway-enter-time-box">
+      <div class="subway-blank"></div>
       <div class="subway-enter-time">
-        시간 입력하기
-
         <v-app id="inspire">
-          <v-time-picker v-model="e7" format="24hr" landscape></v-time-picker>
+          <v-row justify="space-around" align="center">
+            <v-time-picker
+              v-model="picker"
+              :landscape="$vuetify.breakpoint.smAndUp"
+              ampm-in-title
+            ></v-time-picker>
+          </v-row>
         </v-app>
       </div>
-      <div class="subway-show-time">빠른열차보여주는곳</div>
-      <div class="previous-next-time-box">
-        <div>다음열차</div>
-        <div>이전열차</div>
+
+      <div class="subway-time-check">
+        <v-app>
+          <v-btn depressed color="primary"> 시간선택완료 </v-btn>
+        </v-app>
       </div>
+
+      <div class="subway-show-time">
+        <div>이전열차</div>
+        <div>빠른열차 시간보여주는곳</div>
+        <div>다음열차</div>
+      </div>
+      <div class="previous-next-time-box"></div>
     </div>
-    <div class="terminal-map">map</div>
+
+    <div class="terminal-map">
+      <v-app id="inspire">
+        <v-carousel hide-delimiters height="auto">
+          <v-carousel-item
+            v-for="(item, i) in items"
+            :key="i"
+            :src="item.src"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-app>
+    </div>
   </div>
 </template>
 
@@ -26,16 +50,26 @@
 export default {
   data() {
     return {
-      e7: null,
+      picker: null,
+      items: [
+        {
+          src: '/transportation/map_taxi_guide_t1_01.png',
+        },
+        {
+          src: '/transportation/map_taxi_guide_t2_01.png',
+        },
+      ],
     }
+  },
+  methods: {
+    Test() {
+      console.log(this.picker)
+    },
   },
 }
 </script>
 
-<style>
-.v-picker--landscape .v-picker__title {
-  width: 100px;
-}
+<style scoped>
 .subway-box {
   width: 100vw;
   height: 85vh;
@@ -43,7 +77,7 @@ export default {
 }
 .select-terminal-nav {
   height: 100%;
-  width: 20%;
+  width: 15%;
 }
 .terminal-1 {
   width: 100%;
@@ -61,7 +95,7 @@ export default {
 }
 
 .subway-enter-time-box {
-  width: 30%;
+  width: 35%;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -71,17 +105,32 @@ export default {
   height: 40%;
   display: inline-block;
 }
+/* 여기서 뷰티파이 */
+::v-deep .v-application--wrap {
+  min-height: fit-content;
+}
+/* 여기서 뷰티파이 */
 
-.subway-show-time {
-  width: 100%;
-  height: 30%;
+.subway-time-check {
+  height: 8%;
   display: flex;
   justify-content: center;
+  align-items: flex-end;
+}
+.subway-blank {
+  height: 5%;
+  width: 100%;
+}
+.subway-show-time {
+  width: 100%;
+  height: 15%;
+  display: flex;
+  justify-content: space-around;
   align-items: center;
 }
 .previous-next-time-box {
   width: 100%;
-  height: 30%;
+  height: 15%;
   display: flex;
   justify-content: space-around;
   align-items: center;
