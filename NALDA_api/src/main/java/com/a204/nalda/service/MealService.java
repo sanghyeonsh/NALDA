ae3ecd71cdd1ca0554f67446ae8098c807628bd8
ksh3967 <ksh3967@naver.com>
2022-08-17 00:14:02 +0900

    [Edit] meal status ready add

--- NALDA_api/src/main/java/com/a204/nalda/service/MealService.java
+++ NALDA_api/src/main/java/com/a204/nalda/service/MealService.java
@@ -64,7 +64,7 @@ public class MealService {
     public void mealCntInput(List<MealCntDto> mealCntDTOS){
 //        Optional<MealStock> mealValid = mealStockRepository.findTopByFlightNum(mealCntDTOS.get(0).getFlightNum());
         List<MealStock> mealValid = mealStockRepository.findByFlightNum(mealCntDTOS.get(0).getFlightNum());
-        if(mealValid == null){
+        if(mealValid.size()==0){
             for (MealCntDto mealCntDto : mealCntDTOS) {
                 Long mealId = mealRepository.findTopByMealMenu(mealCntDto.getMealMenu()).getId();
                 Meal meal = Meal.builder()
@@ -78,7 +78,7 @@ public class MealService {
                         .meal(meal)
                         .flight(flight)
                         .total(mealCntDto.getTotal())
-                        .status(mealCntDto.getStatus())
+                        .status(Status.READY)
                         .build();
                 mealStockRepository.save(mealStock);
             }
@@ -93,7 +93,9 @@ public class MealService {
     public void updateStatus(String flightNum){
         List<MealStock> mealStocks = mealStockRepository.findByFlightNum(flightNum);
         for (MealStock mealStock : mealStocks) {
-            mealStock.changeStatusInfo(Status.DONE);
+            if(mealStock.getStatus()==Status.PROGRESS){
+                mealStock.changeStatusInfo(Status.DONE);
+            }
         }
         List<SeatMeal> seatMeals = seatMealRepository.findByFlightNum(flightNum);
         for (SeatMeal seatMeal : seatMeals) {
